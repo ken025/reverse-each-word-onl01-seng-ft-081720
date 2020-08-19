3fab212e86c9291890d8e343cd386ad7cc96b1a9
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-19 18:14:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence1)
   
-  original_array = sentence1.split(" ")
+ array = sentence1.split(" ")
   return_array = []
-  original_array.each do|sentence1|
+  array.each do|sentence1|
     return_array << sentence1.reverse
   end
   return_array.join(" ")
