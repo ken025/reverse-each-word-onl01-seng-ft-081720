9f85555a7d8c688795d61df679021922e76afe61
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-19 17:51:34 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence1)
+  
+  original_array = sentence1.split(" ")
+  return_array = []
+  original_array.each do|sentence1|
+    return_array << sentence1.reverse
+  end
+  return_array.join(" ")
+end
+end
